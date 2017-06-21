d2756425addc4bc74af387a91ca64eab2fadaa99
Evgeny Vitkalov <ev.vitkalov@mail.ru>
2017-06-21 17:49:55 +0300

    lab3 sudoku
    generator was added

--- lab3/sudoku.py
+++ lab3/sudoku.py
@@ -1,9 +1,24 @@
+import pprint, random
+
+def group(values, n):
+    """
+    Сгруппировать значения values в список, состоящий из списков по n элементов
+
+    >>> group([1,2,3,4], 2)
+    [[1, 2], [3, 4]]
+    >>> group([1,2,3,4,5,6,7,8,9], 3)
+    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    """
+    new_values=[]
+    for i in range(len(values)//n):
+        new_values.append(values[i*n:i*n+n])
+    return new_values
+
 def read_sudoku(filename):
     """ Прочитать Судоку из указанного файла """
     digits = [c for c in open(filename).read() if c in '123456789.']
     grid = group(digits, 9)
     return grid
-
 
 def display(values):
     """Вывод Судоку """
@@ -15,20 +30,9 @@ def display(values):
             print(line, file=out)
     print('', file=out)
 
+def get_row(values, pos): return values[pos[0]]
 
-def group(values, n):
-    """
-    Сгруппировать значения values в список, состоящий из списков по n элементов
-
-    >>> group([1,2,3,4], 2)
-    [[1, 2], [3, 4]]
-    >>> group([1,2,3,4,5,6,7,8,9], 3)
-    [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    """
-    return [[values[i + j*n] for i in range(n)] for j in range((len(values))//n)]
-
-
-def get_row(values, pos):
+def get_col(values, pos):
     """ Возвращает все значения для номера строки, указанной в pos
 
     >>> get_row([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
@@ -38,34 +42,20 @@ def get_row(values, pos):
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    return values[pos[0]]
-
-
-def get_col(values, pos):
-    """ Возвращает все значения для номера столбца, указанного в pos
-
-    >>> get_col([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']], (0, 0))
-    ['1', '4', '7']
-    >>> get_col([['1', '2', '3'], ['4', '.', '6'], ['7', '8', '9']], (0, 1))
-    ['2', '.', '8']
-    >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
-    ['3', '6', '9']
-    """
-    return [raw[pos[1]] for raw in values]
-
+    new_values = []
+    for i in range(len(values)):
+        new_values.append(values[i][pos[1]])
+    return new_values
 
 def get_block(values, pos):
     """ Возвращает все значения из квадрата, в который попадает позиция pos """
-    kv=(pos[0]//3)*3+pos[1]//3
-    lst=[]
-    for i in range(len(grid[0])):
-        for j in range(len(grid)):
-            if ((i//3)*3+j//3)==kv:
-                lst.append(values[i][j])
-    return lst
+    new_values = []
+    for i in range(3):
+        for j in range(3):
+            new_values.append(values[3*(pos[0]//3)+i][3*(pos[1]//3)+j])
+    return new_values
 
-
-def find_empty_position(grid):
+def find_empty_positions(grid):
     """ Найти первую свободную позицию в пазле
 
     >>> find_empty_position([['1', '2', '.'], ['4', '5', '6'], ['7', '8', '9']])
@@ -75,34 +65,19 @@ def find_empty_position(grid):
     >>> find_empty_position([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    for i in range(len(grid[0])):
-        for j in range(len(grid)):
-            if grid[i][j]=='.':
-                return (i, j)
-    return (-1, -1)
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]=='.': return (i, j)
 
 def find_possible_values(grid, pos):
     """ Вернуть все возможные значения для указанной позиции """
-    poss_vals=set([str(i) for i in range(1, 10)])
-    poss_vals=poss_vals.difference(get_col(grid, pos)).difference(get_block(grid, pos)).difference(get_row(grid, pos)) 
-    if len(poss_vals)==1:
-        return poss_vals.pop()
-    else:
-        return poss_vals
+    poss_vals = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    row = get_row(grid, pos)
+    col = get_col(grid, pos)
+    block = get_block(grid, pos)
+    return sorted(list(set(poss_vals) - set(row) - set(col) - set(block)))
 
-
-def check_solution(solution):
-    """ Если решение solution верно, то вернуть True, в противном случае False """
-    for i in range(len(solution[0])):
-        for j in range(len(solution)):
-            box, solution[i][j] = solution[i][j], '.'
-            if not ( box in find_possible_values(solution, (i, j)) ):
-                solution[i][j]=box
-                return False
-            solution[i][j]=box
-    return True
-
-
+solved=0
 def solve(grid):
     """ Решение пазла, заданного в grid """
     """ Как решать Судоку?
@@ -112,24 +87,43 @@ def solve(grid):
             3.1. Поместить это значение на эту позицию
             3.2. Продолжить решать оставшуюся часть пазла
     """
-    curr_pos=find_empty_position(grid)
-    curr_vals=find_possible_values(grid, curr_pos)
-    if curr_pos==(-1, -1):
-        if check_solution(grid):
-            return grid
+    empty = find_empty_positions(grid)
+    if not empty:
+        global solved
+        solved+=1
         return grid
-    elif not curr_vals:
-        return grid
-    for val in curr_vals:
-        grid[curr_pos[0]][curr_pos[1]]=val
-        solution=solve(grid)
-        if find_empty_position(grid)==(-1, -1):
+    poss = find_possible_values(grid, empty)
+    for i in range(len(poss)):
+        grid[empty[0]][empty[1]] = poss[i]
+        if solve(grid):
             return grid
-            if check_solution(solution):
-                return grid
         else:
-            grid[curr_pos[0]][curr_pos[1]]='.'
+            grid[empty[0]][empty[1]] = '.'
+    return None
 
+def generate_sudoku(n):
+    a = [7,8,9,1,2,3,4,5,6]
+    grid=[]
+    for i in range(9):
+        if i!=(3 or 6):
+            for k in range(3):
+                box = a[0]
+                for j in range(len(a)-1):
+                    a[j]=str(a[j+1])
+                a[-1]=str(box)
+            print(a)
+            grid.append(a)
+        else:
+            for k in range(4):
+                box = a[0]
+                for j in range(len(a)-1):
+                    a[j]=str(a[j+1])
+                a[-1]=str(box)
+            print(a)
+            grid.append(a)
+	print(grid)
+    return grid
+	
 if __name__ == '__main__':
     out=open("output.txt", mode='w')
     for fname in ['puzzle1.txt', 'puzzle2.txt', 'puzzle3.txt']:
